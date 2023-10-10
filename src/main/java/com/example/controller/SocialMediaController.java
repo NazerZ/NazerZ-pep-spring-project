7fd8f4cf7131a7039cb3c22ded9ae593339cce64
Nazer Zahir <nazersemail@gmail.com>
2023-10-10 17:32:15 +0000

    v0.1

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,17 @@
 package com.example.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entity.Account;
+import com.example.entity.Message;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +19,45 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+
+@RestController
 public class SocialMediaController {
 
+    /**
+     * @param account
+     * @return
+     */
+    @PostMapping("/register/")
+    public Account postAccountRegister(@RequestBody Account account){
+        return account;
+    }
+
+    @PostMapping("/login")
+    public Account postAccountLogin(@RequestBody Account account){
+        return account;
+    }
+    
+    @PostMapping("/messages/")
+    public Message postMessage(@RequestBody Message message){
+        return message;
+    }
+
+    @GetMapping("/messages/")
+    public List<Message> getAllMessages(){
+        return null;
+    }
+
+    @GetMapping("/messages/{message_id}")
+    public Message getMessageById(@PathVariable int id){
+        return null;
+    }
+    @DeleteMapping("/messages/{message_id}")
+    public Message deleteMessageById(@PathVariable int id){
+        return null;
+    }
+
+    @PatchMapping("/messages/{message_id}")
+    public Message patchMessageById(@RequestBody String text, @PathVariable int id ){
+        return null;
+    }
 }
